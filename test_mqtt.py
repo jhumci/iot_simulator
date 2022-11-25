fc9564c3e9f6da81bbe19c5389ef48a8321f82b7
jhumci <julian.huber@mci.edu>
2022-11-25 16:18:30 +0100

    mqtt works

--- test_mqtt.py
+++ test_mqtt.py
@@ -2,6 +2,7 @@
 
 from mqtt import MqttClientHandler
 import mqtt_credentials
+import time
 
 # %%
 
@@ -9,5 +10,12 @@ mqtt_client_handler = MqttClientHandler("IoT-Simulator",mqtt_credentials.MQTT_BR
 
 # %%
 
-mqtt_client_handler.publish_payload( topic = "Test", payload ="test")
+mqtt_client_handler.publish_payload( topic = "Test", payload ="test22")
 # %%
+
+
+counter = 0
+
+while True:
+    mqtt_client_handler.publish_payload( topic = "Test", payload =str(counter))
+    time.sleep(10)
