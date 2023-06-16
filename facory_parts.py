bc59a8dd15413a4658195a7a1a39c8c88a554644
jhumci <julian.huber@mci.edu>
2023-06-16 15:05:05 +0200

    added varibales

--- facory_parts.py
+++ facory_parts.py
@@ -21,6 +21,32 @@ class dispenser(object):
     self.mqtt_client = mqtt_client
     self.additional_variance = np.random.uniform(1,2) 
 
+  def generate_temperature(self, unix_time, amplitude, period, noise_level):
+      """
+      Generates a sine-shaped temperature profile with a daily pattern based on Unix time,
+      with added random noise.
+
+      Args:
+          unix_time (float or numpy array): Unix time in seconds.
+          amplitude (float): Amplitude of the sine wave.
+          period (float): Period of the sine wave in seconds.
+          noise_level (float): Magnitude of the random noise.
+
+      Returns:
+          float or numpy array: Simulated temperature value(s) with noise.
+      """
+      # Convert Unix time to radians based on the period
+      theta = 2 * np.pi * unix_time / period
+
+      # Generate the sine wave
+      temperature = amplitude * np.sin(theta) + 25
+
+      # Add random noise to the temperature
+      noise = np.random.normal(scale=noise_level)
+      temperature += noise
+
+      return temperature
+
   def get_fill_amount(self, amount_grams):
 
     # If there is still something in there
@@ -51,14 +77,28 @@ class dispenser(object):
     # yield self.env.process(dispenser_1.fill(bottle.recipe.color_levels_grams[dispenser_1.color] )) & self.env.process(dispenser_2.fill(bottle.recipe.color_levels_grams[dispenser_1.color] )) & self.env.process(dispenser_3.fill(bottle.recipe.color_levels_grams[dispenser_2.color] ))
     yield self.env.timeout(config.TIME_FOR_SLOWEST_STATION)
     logging.info(self.iot_message(self.env, bottle)) 
-    self.mqtt_client.publish_payload("dispenser_" + self.color, self.iot_message(self.env, bottle))
 
+    self.temperature = self.generate_temperature(int(time.time()), 10,  24 * 60 * 60, 1)
+
+    self.mqtt_client.publish_payload("dispenser_" + self.color, self.iot_message(self.env, bottle))
+    self.mqtt_client.publish_payload("temperature", self.iot_message_temp(self.env, self.temperature))
+    self.mqtt_client.publish_payload("dispenser_" + self.color + "/vibration", self.iot_message_vibration(self.env, bottle))
     #yield env.timeout(self.get_fill_amount(bottle.recipe.color_levels_grams[self.color]))
     
     yield self.env.timeout(config.TIME_MOVEMENT)
 
+  def compute_vibration(self, fill_amount, temperature_c, fill_level):
+    return fill_amount * 4.1 * (1 *  (20 -temperature_c)) - fill_level/200 + 10
+
+
   def iot_message(self,env, bottle):
     return '{{"dispenser": "{}","bottle": "{}", "time" : {}, "fill_level_grams" : {}}}'.format(self.color, bottle.id, int(time.time()), self.fill_level_grams)
+
+  def iot_message_temp(self,env, temperature):
+    return '{{"time" : {}, "temperature_C" : {}}}'.format(int(time.time()), temperature)
+
+  def iot_message_vibration(self,env, bottle):
+    return '{{"dispenser": "{}","bottle": "{}", "time" : {}, "vibration-index" : {}}}'.format(self.color, bottle.id, int(time.time()), self.compute_vibration(bottle.color_levels_grams[self.color] ,self.temperature, self.fill_level_grams))
 
 
 class Conveyor(object):
